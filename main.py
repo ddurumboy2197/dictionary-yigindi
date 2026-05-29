f06724f92548ddd47aa8cee871de6f58414ed448
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-29 23:23:12 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,6 @@
+def dictionary_values_sum(dictionary):
+    return sum(dictionary.values())
+
+# Misol:
+dictionary = {"a": 1, "b": 2, "c": 3}
+print(dictionary_values_sum(dictionary))  # Chiqaradi: 6
